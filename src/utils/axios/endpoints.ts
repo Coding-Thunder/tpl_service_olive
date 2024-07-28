6ba208a0c30afb56935b26c9ece5d8616537bd22
Vinay Maheshwari <vinaymaheshwari35@gmail.com>
2024-07-28 12:32:55 +0530

    ADD : Request's endpoints

---
+++ src/utils/axios/endpoints.ts
@@ -0,0 +1,11 @@
+const endPoints = {
+    "/home-banner": "/home-banner",
+    "/who-we-are": "/who-we-are",
+    "/message-from-bod": "/message-from-bod",
+    "/services-we-offer": "/services-we-offer",
+    "/sectors-we-operate": "/sectors-we-operate",
+    "/sustainability": "/sustainability"
+}
+
+
+export default endPoints
